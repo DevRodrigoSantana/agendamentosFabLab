6874dcb3b12c797a39ea3aeebe051644aa36c0fb
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2025-10-17 11:52:24 -0300

    commit initial

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule],
+  template: `<router-outlet></router-outlet>`
+})
+export class App {
+  protected readonly title = signal('gerencie_ja');
+}
